f47946cf5949d70960ff8160a4fd49db37aa9da6
Linear <17817363770@163.com>
2017-12-05 23:41:37 +0800

    Add login input button

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,19 +1,44 @@
 <template>
   <div class="login">
-
+    <div class="input_position clearfix">
+      <el-col :span="4">
+        <el-input v-model="account" placeholder="Enter something..."></el-input>
+      </el-col>
+    </div>
+    <div class="input_position clearfix">
+      <el-col :span="4">
+        <el-input v-model="password" placeholder="Enter something..."></el-input>
+      </el-col>
+    </div>
+    <div class="input_position clearfix">
+      <el-col :span="4">
+        <el-button type="success" long @click="login">SUBMIT</el-button>
+      </el-col>
+    </div>
+   
+    
   </div>
 </template>
-a
+
 <script>
-
-
 export default {
   name: 'login',
   components:{
 
+  },
+  data(){
+    return{
+      account:"",
+      password:""
+    }
+  },
+  methods:{
+    login(){
+      
+    }
   }
 }
 </script>
 <style scroped>
-
+.input_position{padding:20px;}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuex from 'vuex'
+import ElementUI from 'element-ui'
 
-import iView from 'iview';
-import 'iview/dist/styles/iview.css';
 import "./assets/css/common.css"
+import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(iView);
+Vue.use(ElementUI);
 Vue.use(Vuex)
 
 Vue.config.productionTip = false
